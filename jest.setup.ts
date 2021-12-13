110f1eb30d31627f1d0f3abca66623f43bca6a65
Martin Brännstrand <martin.brannstrand@publit.se>
2021-12-13 15:27:39 +0100

    Adjusts test setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,14 +2,3 @@ import 'whatwg-fetch'
 import fetchMock from 'jest-fetch-mock'
 
 fetchMock.enableMocks()
-
-const globalAny: any = global
-
-globalAny.ResizeObserver = class {
-  observe() {
-    /* */
-  }
-  unobserve() {
-    /* */
-  }
-}
